29f05ffee52e615d0e4c6c24c7f41d9ca8ac5a2e
Jake <87922888+JakeOJeff@users.noreply.github.com>
2024-01-06 11:54:37 +0530

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
 local SceneryInit = require("scenery")
 local scenery = SceneryInit(
     { path = "src.menu"; key = "menu" },
-    { path = "src.game"; key = "game" }
+    { path = "src.game"; key = "game" },
+    { path = "src.endgame"; key = "endgame"}
 )
 scenery:hook(love)
