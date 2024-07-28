daf34d364670be27a180b826d7323e87d12ef212
Jake <87922888+JakeOJeff@users.noreply.github.com>
2024-07-28 15:07:15 +0530

    Add files via upload

---
+++ assets/audio/songs/Taylor Swift - 22/lyrics.lua
@@ -0,0 +1,20 @@
+local lyrics = {
+    {text = "[MUSIC]", timestamp = 0},
+    {text = "It feels like a perfect night", timestamp = 4.5},
+    {text = "To dress up like hipsters", timestamp = 6.4 },
+    {text = "Make fun of our exes", timestamp = 9 },
+    {text = "Aw Oh, Aw Oh", timestamp = 11.5 },
+    {text = "It feels like a perfect night", timestamp = 13 },
+    {text = "For breakfast at midnight", timestamp = 15.5 },
+    {text = "To fall in love with strangers", timestamp = 18 },
+    {text = "Aw Oh, Aw Oh", timestamp = 21 },
+    {text = "Yeahhhhhhh", timestamp = 23 },
+    {text = "We're happy, free, confused ", timestamp = 25 },
+    {text = "and lonely at the same time", timestamp = 26.5 },
+    {text = "It's a miserable and magical", timestamp = 28.5 },
+    {text = "ohh, yeahhhhhh", timestamp = 34 },
+    {text = "Tonights the night when we forget", timestamp = 36 },
+    {text = "about the deadlines", timestamp = 38 },
+}
+
+return lyrics
